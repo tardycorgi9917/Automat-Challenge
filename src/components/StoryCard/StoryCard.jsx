c3a5345c7a268413e1fd70765ea2154f567aee59
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2018-02-27 12:46:40 -0500

    starting to look like something

--- src/components/StoryCard/StoryCard.jsx
+++ src/components/StoryCard/StoryCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import moment from 'moment';
 
 const StoryCard = ({
     by,
@@ -9,7 +10,11 @@ const StoryCard = ({
     url,
 }) =>
     <div className={"story-card-container"}>
-        <p className={"story-title"}>{title}</p>
+        <div className={'story-points'}><p>{score}</p></div>
+        <div className={"story-info"}>
+            <p className={"story-title"}>{title}</p>
+            <p>{moment(time, "X").fromNow()}</p>
+        </div>
     </div>
 
 export default StoryCard;

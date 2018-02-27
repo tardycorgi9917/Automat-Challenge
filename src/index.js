c7067ff4ff594d8298ea2885e5b1dfdea6f89c5b
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2018-02-27 10:45:01 -0500

    added redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/storiesReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    stories: []
+}
+
+export default (state = initialState, action = {}) => {
+    switch(action.type){
+        
+    }
+}

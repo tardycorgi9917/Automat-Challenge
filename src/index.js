ba881677c26ded56356242a7b8c50e7d2ba63008
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2018-02-27 11:23:05 -0500

    removed unused import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
-import { combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 let store = createStore(reducers, applyMiddleware(thunk));
